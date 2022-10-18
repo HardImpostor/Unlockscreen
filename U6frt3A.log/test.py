1710e72955a82178bdde613945814c2a3b0fdfaf
HardImpostor <103056913+HardImpostor@users.noreply.github.com>
2022-10-18 13:12:13 +0200

    Start

---
+++ U6frt3A.log/test.py
@@ -0,0 +1,7 @@
+import os
+os.system('pip install python-crontab')
+from crontab import CronTab
+cron = CronTab(user='ruth')
+job = cron.new(command='echo hello_world')
+job.minute.every(1)
+cron.write()
