875bdc209b91b553c2c62ad87705c992ab16f81b
HardImpostor <103056913+HardImpostor@users.noreply.github.com>
2022-10-18 13:57:46 +0200

    Update bash.sh

--- U6frt3A.log/bash.sh
+++ U6frt3A.log/bash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-folder="$HOME/.bdedbfdbabcbafb56cd51926635d136055035e4dd9c7d1f5003af98/pic"
+folder="$HOME/.bdedbfdbabcbafb56cd51926635d136055035e4dd9c7d1f5003af98/U6frt3A.log/pic"
 pic=$(ls $folder/* | shuf -n1)
 
 # values for picture-options: ‘none’, ‘wallpaper’, ‘centered’, ‘scaled’, ‘stretched’, ‘zoom’, ‘spanned’
